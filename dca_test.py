f31a6f478140c80afdbc421c395d426a14ba559f
Logikable <spamseanluchen@gmail.com>
2017-08-04 10:19:56 -0400

    removed quotations

--- dca_test.py
+++ dca_test.py
@@ -7,6 +7,6 @@ import time
 time.sleep(5)
 
 for i in range(10):
-	call(['./dca', 'transaction', 'charge', '-p', 'p1', '-e', '5', '-j', '5', '-s', '"2017-08-04 10:10:00"', '-m'])
+	call(['./dca', 'transaction', 'charge', '-p', 'p1', '-e', '5', '-j', '5', '-s', '2017-08-04 10:10:00', '-m'])
 
 call(['./dca', 'bill', 'generate', '-t', 't2', '--time_period', 'last_day'])
